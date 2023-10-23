c138e1259c521ce8f462580d99c41916e64d74ea
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2023-10-22 18:28:39 -0700

    Add files via upload

---
+++ ComposingViews/ComposingViews/ComposingViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposingViewsApp.swift
+//  ComposingViews
+//
+//  Created by Brandon Johns on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ComposingViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
